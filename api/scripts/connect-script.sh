a6a5bba4888ec09903cee57bff1f89e2edaad4fe
Lad Ab <laddev.site@inbox.lv>
2023-08-12 18:35:52 +0300

    Bug fixed

--- api/scripts/connect-script.sh
+++ api/scripts/connect-script.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-## Путь к файлу для сохранения данных
-#LOGFILE="/root/OcServNode/api/connect_log.txt"
-#
-## Дата и время вызова скрипта
-#echo "Script invoked at $(date)" >> $LOGFILE
-#
-## Печать всех переменных окружения в файл
-#printenv >> $LOGFILE
-#
-## Добавить разделитель для удобства чтения
-#echo "-----------------------------------" >> $LOGFILE
+# Путь к файлу для сохранения данных
+LOGFILE="/root/OcServNode/api/connect_log.txt"
+
+# Дата и время вызова скрипта
+echo "Script invoked at $(date)" >> $LOGFILE
+
+# Печать всех переменных окружения в файл
+printenv >> $LOGFILE
+
+# Добавить разделитель для удобства чтения
+echo "-----------------------------------" >> $LOGFILE
 
 
 node user_connect.js "$@"
 if [ $? -eq 1 ]; then
-  echo "1"
     exit 1
 else
-  echo "0"
     exit 0
 fi
