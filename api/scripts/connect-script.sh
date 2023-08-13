43e2c2a96f5e40112606f37b1b5c62bb51d69692
Lad Ab <laddev.site@inbox.lv>
2023-08-13 07:18:13 +0300

    Bug fixed

--- api/scripts/connect-script.sh
+++ api/scripts/connect-script.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# Путь к файлу для сохранения данных
-LOGFILE="/root/OcServNode/api/connect_log.txt"
+## Путь к файлу для сохранения данных
+#LOGFILE="/root/OcServNode/api/connect_log.txt"
+#
+## Дата и время вызова скрипта
+#echo "Script invoked at $(date)" >> $LOGFILE
+#
+## Печать всех переменных окружения в файл
+#printenv >> $LOGFILE
+#
+## Добавить разделитель для удобства чтения
+#echo "-----------------------------------" >> $LOGFILE
 
-# Дата и время вызова скрипта
-echo "Script invoked at $(date)" >> $LOGFILE
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Печать всех переменных окружения в файл
-printenv >> $LOGFILE
-
-# Добавить разделитель для удобства чтения
-echo "-----------------------------------" >> $LOGFILE
-
-
-/bin/node /root/OcServNode/api/scripts/user_connect.js USERNAME=$USERNAME INVOCATION_ID=$INVOCATION_ID GROUPNAME=$GROUPNAME DEVICE=$DEVICE IP_REAL=$IP_REAL IP_REMOTE=$IP_REMOTE IP_REAL_LOCAL=$IP_REAL_LOCAL ID=$ID VHOST=$VHOST HOSTNAME=$HOSTNAME
+/bin/node /bin/node "$DIR/user_connect.js" USERNAME=$USERNAME INVOCATION_ID=$INVOCATION_ID GROUPNAME=$GROUPNAME DEVICE=$DEVICE IP_REAL=$IP_REAL IP_REMOTE=$IP_REMOTE IP_REAL_LOCAL=$IP_REAL_LOCAL ID=$ID VHOST=$VHOST HOSTNAME=$HOSTNAME
 if [ $? -eq 1 ]; then
     exit 1
 else
--- api/scripts/disconnect-script.sh
+++ api/scripts/disconnect-script.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# Путь к файлу для сохранения данных
-LOGFILE="/root/OcServNode/api/connect_log.txt"
+## Путь к файлу для сохранения данных
+#LOGFILE="/root/OcServNode/api/connect_log.txt"
+#
+## Дата и время вызова скрипта
+#echo "Script invoked at $(date)" >> $LOGFILE
+#
+## Печать всех переменных окружения в файл
+#printenv >> $LOGFILE
+#
+## Добавить разделитель для удобства чтения
+#echo "-----------------------------------" >> $LOGFILE
 
-# Дата и время вызова скрипта
-echo "Script invoked at $(date)" >> $LOGFILE
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Печать всех переменных окружения в файл
-printenv >> $LOGFILE
-
-# Добавить разделитель для удобства чтения
-echo "-----------------------------------" >> $LOGFILE
-
-
-/bin/node /root/OcServNode/api/scripts/user_disconnect.js USERNAME=$USERNAME INVOCATION_ID=$INVOCATION_ID GROUPNAME=$GROUPNAME DEVICE=$DEVICE IP_REAL=$IP_REAL IP_REMOTE=$IP_REMOTE IP_REAL_LOCAL=$IP_REAL_LOCAL ID=$ID VHOST=$VHOST STATS_BYTES_OUT=$STATS_BYTES_OUT STATS_BYTES_IN=$STATS_BYTES_IN HOSTNAME=$HOSTNAME
+/bin/node "$DIR/user_disconnect.js" USERNAME=$USERNAME INVOCATION_ID=$INVOCATION_ID GROUPNAME=$GROUPNAME DEVICE=$DEVICE IP_REAL=$IP_REAL IP_REMOTE=$IP_REMOTE IP_REAL_LOCAL=$IP_REAL_LOCAL ID=$ID VHOST=$VHOST STATS_BYTES_OUT=$STATS_BYTES_OUT STATS_BYTES_IN=$STATS_BYTES_IN HOSTNAME=$HOSTNAME
 if [ $? -eq 1 ]; then
     exit 1
 else
