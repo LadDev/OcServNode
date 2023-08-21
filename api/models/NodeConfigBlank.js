63ba2c9377c6eec9847c3f5b327308c8e7f7f7da
Lad Ab <laddev.site@inbox.lv>
2023-08-21 14:40:00 +0300

    Added users cync

--- api/models/NodeConfigBlank.js
+++ api/models/NodeConfigBlank.js
@@ -1,7 +1,10 @@
 const {Schema, model} = require('mongoose')
 const schema = new Schema({
     name: {type: String, required: true},
-    config: {type: Object, required: true},
+    groupName: {type: String, default: ""},
+    isDefault: {type: Boolean, default: false},
+    isSubscription: {type: Boolean, default: false},
+    config: {type: String, default: ""},
     created: {type: Date, default: Date.now},
 })
 
--- install.sh
+++ install.sh
@@ -60,6 +60,10 @@ cd "$current_dir/api" && pm2 start pm2.config.js
 cd "$current_dir/api" && sudo chmod +x scripts/connect-script.sh
 cd "$current_dir/api" && sudo chmod +x scripts/disconnect-script.sh
 
+sudo mkdir -p /var/ocserv
+sudo mkdir -p /var/ocserv/clients
+sudo mkdir -p /var/ocserv/groups
+
 iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o eth0 -j MASQUERADE; iptables -A FORWARD -s 10.0.0.0/24 -j ACCEPT; iptables -A FORWARD -d 10.0.0.0/24 -j ACCEPT
 
 
