0a19255f560743f72b616369250c7fc7838760e6
Lad Ab <laddev.site@inbox.lv>
2023-09-07 15:40:30 +0300

    Optimized code

--- api/scripts/user_disconnect.js
+++ api/scripts/user_disconnect.js
@@ -1,63 +1,88 @@
 const UsersOnline = require("../models/UsersOnline");
-const { config } = require('dotenv');
+const {config} = require('dotenv');
 const Sessions = require("../models/Sessions");
-const { dbConnect } = require("../db.connector");
+const DIR = `${__dirname}`.replace("/scripts", "")
+config({path: `${DIR}/.env`});
+const {dbConnect} = require("../db.connector");
 const Nodes = require("../models/Nodes");
 
-const DIR = `${__dirname}`.replace("/scripts", "");
-config({ path: `${DIR}/.env` });
-
 (async () => {
-    await dbConnect();
+    await dbConnect()
 
-    const argMap = {};
-    process.argv.forEach(arg => {
-        const [key, value] = arg.split("=");
-        argMap[key] = value;
-    });
+    let USERNAME = null
+    let INVOCATION_ID = null
+    let GROUPNAME = null
+    let DEVICE = null
+    let IP_REAL = null
+    let IP_REMOTE = null
+    let IP_REAL_LOCAL = null
+    let ID = null
+    let VHOST = null
+    let STATS_BYTES_OUT = 0
+    let STATS_BYTES_IN = 0
+    for (const arg of process.argv) {
+        const argArrTmp = arg.split("=")
+        if (argArrTmp[0].startsWith("INVOCATION_ID")) {
+            INVOCATION_ID = argArrTmp[1]
+        } else if (argArrTmp[0].startsWith("USERNAME")) {
+            USERNAME = argArrTmp[1]
+        } else if (argArrTmp[0].startsWith("GROUPNAME")) {
+            GROUPNAME = argArrTmp[1]
+        } else if (argArrTmp[0].startsWith("DEVICE")) {
+            DEVICE = argArrTmp[1]
+        } else if (argArrTmp[0].startsWith("IP_REAL")) {
+            IP_REAL = argArrTmp[1]
+        } else if (argArrTmp[0].startsWith("IP_REMOTE")) {
+            IP_REMOTE = argArrTmp[1]
+        } else if (argArrTmp[0].startsWith("ID")) {
+            ID = argArrTmp[1]
+        } else if (argArrTmp[0].startsWith("VHOST")) {
+            VHOST = argArrTmp[1]
+        } else if (argArrTmp[0].startsWith("IP_REAL_LOCAL")) {
+            IP_REAL_LOCAL = argArrTmp[1]
+        } else if (argArrTmp[0].startsWith("STATS_BYTES_OUT")) {
+            STATS_BYTES_OUT = argArrTmp[1]
+        } else if (argArrTmp[0].startsWith("STATS_BYTES_IN")) {
+            STATS_BYTES_IN = argArrTmp[1]
+        }
+    }
 
-    const {
-        USERNAME,
-        INVOCATION_ID,
-        ID,
-        STATS_BYTES_OUT = 0,
-        STATS_BYTES_IN = 0
-    } = argMap;
 
     if (USERNAME && INVOCATION_ID) {
-        const uo = await UsersOnline.findOne({ sesId: Number(ID), userName: USERNAME, invocationId: INVOCATION_ID });
+        const uo = await UsersOnline.findOne({sesId:Number(ID), userName: USERNAME, invocationId: INVOCATION_ID})
         if (uo) {
-            Object.assign(uo, {
-                statsBytesIn: Number(STATS_BYTES_IN) + uo.statsBytesIn,
-                statsBytesOut: Number(STATS_BYTES_OUT) + uo.statsBytesOut,
-                status: "disconnect",
-                sesId: null,
-                disconnectAt: new Date().toISOString()
-            });
-            await uo.save();
+            uo.statsBytesIn = Number(STATS_BYTES_IN)+uo.statsBytesIn
+            uo.statsBytesOut = Number(STATS_BYTES_OUT)+uo.statsBytesOut
+            uo.status = "disconnect"
+            uo.sesId = null
+            uo.disconnectAt = new Date().toISOString()
+            await uo.save()
 
-            const sessionDB = await Sessions.findOne({ uuid: uo.uuid, uid: uo.uid, userName: uo.userName, session: uo.session, fullSession: uo.fullSession });
-            Object.assign(sessionDB, {
-                connected: false,
-                session_is_open: 0,
-                in_use: 0,
-                closed: new Date().toISOString(),
-                statsBytesIn: Number(STATS_BYTES_IN),
-                statsBytesOut: Number(STATS_BYTES_OUT)
-            });
-            await sessionDB.save();
+            const sessionDB = await Sessions.findOne({uuid: uo.uuid, uid: uo.uid,userName: uo.userName, session: uo.session, fullSession: uo.fullSession});
+            sessionDB.connected = false
+            sessionDB.session_is_open = 0
+            sessionDB.in_use = 0
+            sessionDB.closed = new Date().toISOString()
+            sessionDB.statsBytesIn = Number(STATS_BYTES_IN)
+            sessionDB.statsBytesOut = Number(STATS_BYTES_OUT)
+            await sessionDB.save()
         }
 
-        try {
-            const node = await Nodes.findOne({ uuid: process.env.UUID });
-            if (node && node.status && node.status.occtlStatus) {
-                node.status.occtlStatus.activesessions = Math.max(node.status.occtlStatus.activesessions - 1, 0);
-                node.status.occtlStatus.totalsessions = Math.max(node.status.occtlStatus.totalsessions - 1, 0);
-                await node.save();
+        try{
+            const node = await Nodes.findOne({uuid: process.env.UUID})
+            if(node && node.status && node.status.occtlStatus && node.status.occtlStatus !== {}){
+                if(node.status.occtlStatus.activesessions > 0) {
+                    node.status.occtlStatus.activesessions -= 1
+                }
+                if(node.status.occtlStatus.totalsessions > 0) {
+                    node.status.occtlStatus.totalsessions -= 1
+                }
+                await node.save()
             }
-        } catch (e) { /* handle error if necessary */ }
+        }catch (e){}
 
-        process.exit(0);
+        process.exit(0)
     }
-    process.exit(1);
+    process.exit(1)
+
 })();
