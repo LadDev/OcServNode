f303fd953b265152e57fa25c693a3581c6e94622
Lad Ab <laddev.site@inbox.lv>
2023-09-07 15:37:42 +0300

    Optimized code

--- api/scripts/user_connect.js
+++ api/scripts/user_connect.js
@@ -1,147 +1,111 @@
 const Users = require("../models/Users");
 const UsersOnline = require("../models/UsersOnline");
-const {config} = require('dotenv');
-const DIR = `${__dirname}`.replace("/scripts","")
-config({path: `${DIR}/.env`});
-const {dbConnect} = require("../db.connector");
+const { config } = require('dotenv');
+const { dbConnect } = require("../db.connector");
 const OcctlExec = require("../classes/OcctlExec.class");
 const Nodes = require("../models/Nodes");
 const PurchasedSubscription = require("../models/PurchasedSubscription");
 
+const DIR = `${__dirname}`.replace("/scripts", "");
+config({ path: `${DIR}/.env` });
+
 (async () => {
-    await dbConnect()
+    await dbConnect();
 
-    let USERNAME = null
-    let INVOCATION_ID = null
-    let GROUPNAME = null
-    let DEVICE = null
-    let IP_REAL = null
-    let IP_REMOTE = null
-    let IP_REAL_LOCAL = null
-    let ID = null
-    let VHOST = null
-    for (const arg of process.argv) {
-        const argArrTmp = arg.split("=")
-        if (argArrTmp[0].startsWith("INVOCATION_ID")) {
-            INVOCATION_ID = argArrTmp[1]
-        } else if (argArrTmp[0].startsWith("USERNAME")) {
-            USERNAME = argArrTmp[1]
-        } else if (argArrTmp[0].startsWith("GROUPNAME")) {
-            GROUPNAME = argArrTmp[1]
-        } else if (argArrTmp[0].startsWith("DEVICE")) {
-            DEVICE = argArrTmp[1]
-        } else if (argArrTmp[0].startsWith("IP_REAL")) {
-            IP_REAL = argArrTmp[1]
-        } else if (argArrTmp[0].startsWith("IP_REMOTE")) {
-            IP_REMOTE = argArrTmp[1]
-        } else if (argArrTmp[0].startsWith("ID")) {
-            ID = argArrTmp[1]
-        } else if (argArrTmp[0].startsWith("VHOST")) {
-            VHOST = argArrTmp[1]
-        } else if (argArrTmp[0].startsWith("IP_REAL_LOCAL")) {
-            IP_REAL_LOCAL = argArrTmp[1]
-        }
-    }
+    const argMap = {};
+    process.argv.forEach(arg => {
+        const [key, value] = arg.split("=");
+        argMap[key] = value;
+    });
 
+    const {
+        USERNAME,
+        INVOCATION_ID,
+        GROUPNAME,
+        DEVICE,
+        IP_REAL,
+        IP_REMOTE,
+        ID,
+        VHOST,
+        IP_REAL_LOCAL
+    } = argMap;
 
     if (USERNAME) {
-        let sess = null
-        let fullSess = null
-        let tlsciphersuite = null
+        let sess, fullSess, tlsciphersuite;
 
-        const usersConnected = await new OcctlExec().users()
-        if (usersConnected) {
-            for (const uc of usersConnected) {
-                if (Number(uc.id) === Number(ID)) {
-                    sess = uc.session
-                    fullSess = uc.fullsession
-                    tlsciphersuite = uc.tlsciphersuite
-                    break;
-                }
-            }
+        const usersConnected = await new OcctlExec().users();
+        const userConnection = usersConnected.find(uc => Number(uc.id) === Number(ID));
+        if (userConnection) {
+            ({ session: sess, fullsession: fullSess, tlsciphersuite } = userConnection);
         } else {
-            process.exit(0)
+            process.exit(0);
         }
 
-        const user = await Users.findOne({username: USERNAME, enabled: true})
-        if (user) {
+        const user = await Users.findOne({ username: USERNAME, enabled: true });
 
-            try{
-                if(user.client_id){
+        if (user && user.client_id) {
+            const purchased = await PurchasedSubscription.findOne({
+                client_id: user.client_id,
+                activated: true,
+                expired: false
+            });
 
-                    const purchased = await PurchasedSubscription.findOne({client_id: user.client_id, activated: true, expired: false})
-                    if(!purchased){
-                        const purchasedNew = await PurchasedSubscription.findOne({client_id: user.client_id, activated: false, expired: false})
-                        if(purchasedNew){
-                            const now = new Date();
-                            const twoDaysLater = new Date(now);
-                            twoDaysLater.setDate(now.getDate() + Number(purchasedNew.period));
-                            purchasedNew.activated = true
-                            purchasedNew.startDate = now.toISOString()
-                            purchasedNew.endDate = twoDaysLater.toISOString()
-                            await purchasedNew.save()
-                        }
-                    }
-                }
-            }catch (e) {
-
-            }
-
-
-            const uo = await UsersOnline.findOne({userName: USERNAME, invocationId: INVOCATION_ID, sesId: null})
-            if (uo) {
-                uo.uid = user.id
-                uo.sesId = Number(ID)
-                uo.groupName = GROUPNAME
-                uo.userName = USERNAME
-                uo.invocationId = INVOCATION_ID
-                uo.uuid = process.env.UUID
-                uo.device = DEVICE
-                uo.remoteIp = IP_REAL
-                uo.ipv4 = IP_REMOTE
-                uo.vhost = VHOST
-                uo.localDeviceIp = IP_REAL_LOCAL
-                uo.session = sess
-                uo.fullSession = fullSess
-                uo.tlsCiphersuite = tlsciphersuite
-                uo.status = "connect"
-                uo.connectAt = new Date().toISOString()
-                uo.disconnectAt = null
-                await uo.save()
-            } else {
-                const uoNew = new UsersOnline({
-                    uid: user.id,
-                    sesId: Number(ID),
-                    userName: USERNAME,
-                    groupName: GROUPNAME,
-                    invocationId: INVOCATION_ID,
-                    uuid: process.env.UUID,
-                    device: DEVICE,
-                    remoteIp: IP_REAL,
-                    ipv4: IP_REMOTE,
-                    vhost: VHOST,
-                    localDeviceIp: IP_REAL_LOCAL,
-                    status: "connect",
-                    session: sess,
-                    fullSession: fullSess,
-                    tlsCiphersuite: tlsciphersuite,
-                    connectAt: new Date().toISOString()
+            if (!purchased) {
+                const purchasedNew = await PurchasedSubscription.findOne({
+                    client_id: user.client_id,
+                    activated: false,
+                    expired: false
                 });
 
-                await uoNew.save()
-            }
-            try{
-                const node = await Nodes.findOne({uuid: process.env.UUID})
-                if(node && node.status && node.status.occtlStatus && node.status.occtlStatus !== {}){
-                    node.status.occtlStatus.activesessions = usersConnected.length
-                    node.status.occtlStatus.totalsessions = usersConnected.length
-                    await node.save()
+                if (purchasedNew) {
+                    const now = new Date();
+                    const endDate = new Date(now.setDate(now.getDate() + Number(purchasedNew.period)));
+                    Object.assign(purchasedNew, { activated: true, startDate: now, endDate });
+                    await purchasedNew.save();
                 }
-            }catch (e){}
-
-            process.exit(0)
+            }
         }
-    }
-    process.exit(1)
 
+        const commonFields = {
+            uid: user.id,
+            sesId: Number(ID),
+            userName: USERNAME,
+            groupName: GROUPNAME,
+            invocationId: INVOCATION_ID,
+            uuid: process.env.UUID,
+            device: DEVICE,
+            remoteIp: IP_REAL,
+            ipv4: IP_REMOTE,
+            vhost: VHOST,
+            localDeviceIp: IP_REAL_LOCAL,
+            session: sess,
+            fullSession: fullSess,
+            tlsCiphersuite,
+            status: "connect",
+            connectAt: new Date().toISOString()
+        };
+
+        let uo = await UsersOnline.findOne({ userName: USERNAME, invocationId: INVOCATION_ID, sesId: null });
+
+        if (uo) {
+            Object.assign(uo, commonFields, { disconnectAt: null });
+            await uo.save();
+        } else {
+            const uoNew = new UsersOnline(commonFields);
+            await uoNew.save();
+        }
+
+        try {
+            const node = await Nodes.findOne({ uuid: process.env.UUID });
+            if (node && node.status && node.status.occtlStatus) {
+                node.status.occtlStatus.activesessions = usersConnected.length;
+                node.status.occtlStatus.totalsessions = usersConnected.length;
+                await node.save();
+            }
+        } catch (e) { /* handle error if necessary */ }
+
+        process.exit(0);
+    }
+    process.exit(1);
 })();
+
